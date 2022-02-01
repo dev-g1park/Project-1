f7a474b5426bcbba9c4b616f4dceef2525a51666
Jiwon Park <g1park1201@gmail.com>
2022-02-01 19:48:59 +0900

    third commit

--- 4_2_text.py
+++ 4_2_text.py
@@ -11,3 +11,4 @@ print("len('hello world'*1000)", len('hello world'*1000))
 print("'hello world'.replace('world', 'universe')", 'hello world'.replace('world', 'universe'))
 
 아니 ㅅㅂ master랑 main이랑 뭐가 다른 건데
+g1park-dec1으로 바꿔도 되나
