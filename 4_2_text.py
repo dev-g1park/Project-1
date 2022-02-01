adb9b03b4cea15074e054884f4dee95a321ec49f
Jiwon Park <g1park1201@gmail.com>
2022-02-01 19:34:24 +0900

    second commit

--- 4_2_text.py
+++ 4_2_text.py
@@ -9,3 +9,5 @@ print("'1'+'1'", '1'+'1')
 print('hello world'*1000)
 print("len('hello world'*1000)", len('hello world'*1000))
 print("'hello world'.replace('world', 'universe')", 'hello world'.replace('world', 'universe'))
+
+아니 ㅅㅂ master랑 main이랑 뭐가 다른 건데
